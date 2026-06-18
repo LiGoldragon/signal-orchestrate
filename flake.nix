80911d1aa20a785fddc6be124c13e568a6250402
li <li@goldragon.criome.net>
2026-06-18 19:23:45 +0200

    signal-orchestrate: use shared rust-build source policy

--- flake.nix
+++ flake.nix
@@ -4,34 +4,31 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    fenix = {
-      url = "github:nix-community/fenix";
+    rust-build = {
+      url = "github:LiGoldragon/rust-build";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nixpkgs, flake-utils, fenix, crane }:
+  outputs = { self, nixpkgs, flake-utils, rust-build }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        toolchain = fenix.packages.${system}.fromToolchainFile {
+        rust = rust-build.lib.${system}.fromToolchainFile pkgs {
           file = ./rust-toolchain.toml;
           sha256 = "sha256-gh/xTkxKHL4eiRXzWv8KP7vfjSk61Iq48x47BEDFgfk=";
         };
-        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
-        src = pkgs.lib.cleanSourceWith {
-          src = ./.;
-          filter =
-            path: type:
-            let
-              pathString = toString path;
-              schemaRoot = "${toString ./.}/schema";
-            in
-            craneLib.filterCargoSources path type
-            || pathString == schemaRoot
-            || pkgs.lib.hasPrefix "${schemaRoot}/" pathString;
-          name = "source";
+        inherit (rust) craneLib toolchain;
+        schemaFilter =
+          path: _type:
+          let
+            pathString = toString path;
+            schemaRoot = "${toString ./.}/schema";
+          in
+          pathString == schemaRoot || pkgs.lib.hasPrefix "${schemaRoot}/" pathString;
+        src = rust.cleanSource {
+          root = ./.;
+          extraFilters = [ schemaFilter ];
         };
         commonArgs = { inherit src; strictDeps = true; };
         cargoArtifacts = craneLib.buildDepsOnly commonArgs;
